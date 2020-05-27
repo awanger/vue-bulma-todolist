d4db8d1eec51a4728c84997545c7602388da18f1
Alex Wang <alexwangnj@gmail.com>
2020-05-27 12:44:46 -0400

    Add todos component

---
+++ src/components/Todos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="todos">
+    <h1>My Todos Dos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JesusLama'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    font-size: 48px;
+  }
+</style>
